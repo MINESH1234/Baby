d7a9d7b3b4f68e9c0da1d79d7dc2a4743e282c91
MINESH1234 <160322620+MINESH1234@users.noreply.github.com>
2024-07-16 00:02:44 +0530

    Update Anox.sh

--- Anox.sh
+++ Anox.sh
@@ -1,13 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-# Update package list and upgrade all packages
-pkg update -y
-pkg upgrade -y
-
-# Install Python
-pkg install python -y
-
-# Clone the GitHub repository
+#
 git clone https://github.com/MINESH1234/create.git
 # Navigate to the cloned repository
 cd create
